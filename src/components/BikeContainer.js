01d3511d777a52299ebc16217d406849344c9794
Jayaprakashs-AdapptLimited <jayaprakash.s@adappt.ai>
2023-08-23 19:32:03 +0530

    Initial Commit

---
+++ src/components/BikeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BikeContainer() {
+  return (
+    <div>
+      <h1> Number of Bikes </h1>
+      <button> Buy Bike </button>
+    </div>
+  )
+}
+
+export default BikeContainer;
